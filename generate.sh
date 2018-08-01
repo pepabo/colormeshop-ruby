07df1f81f9c7b9c7f889c42615932354432599e4
ackintosh <sora.akatsuki@gmail.com>
2018-08-01 12:52:07 +0900

    gemのバージョンを指定する
    課金APIを追加したのでマイナーバージョンを上げる
    refs https://github.com/pepabo/colormeshop-ruby/pull/3

--- generate.sh
+++ generate.sh
@@ -5,4 +5,4 @@ docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate \
     -i https://api.shop-pro.jp/v1/swagger.json \
     -g ruby \
     -o /local/ \
-    --additional-properties moduleName=ColorMeShop,gemAuthor="GMO Pepabo inc.",gemHomepage=https://shop-pro.jp,gemLicense=MIT,gemDescription="カラーミーショップAPIのRubyクライアントです。"
+    --additional-properties moduleName=ColorMeShop,gemAuthor="GMO Pepabo inc.",gemHomepage=https://shop-pro.jp,gemLicense=MIT,gemDescription="カラーミーショップAPIのRubyクライアントです。",gemVersion=1.1.0
